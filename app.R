4633be6ca52eaa60a9e362dd881335429685d9c3
Carles Hernandez-Ferrer <kuragari.ch@gmail.com>
2018-06-05 12:07:52 -0400

    Merge with Jeff's version.

--- app.R
+++ app.R
@@ -6,75 +6,78 @@ library(DT)
 library(plotly)
 library(reshape2)
 library(RColorBrewer)
-#install.packages("shinyBS")
+
 # laod functions
 source("function.R")
 
+library(data.table)
+setDTthreads(18)
+
 # load data
-gene_symbol <- readRDS("../data/gene_symbol.rds")
-gnomad_exome <- read.delim("gnomad_exome.txt", header = F, stringsAsFactors = FALSE)
-rownames(gnomad_exome) <- gnomad_exome$V1
-summary <- readRDS("../data/summary.rds")
-gtrM <- read.delim("GTR_table.txt")
-gtrS <- read.delim("GTR_summary.txt")
-rownames(gtrS) <- gtrS$ccds_id
-pheno <- readRDS("../data/phenotypes.rds")
-genes_by_ccds_id <- readRDS('../data/genes_by_ccds_id.rds')
+library(fst)
 
-#master <- readRDS("../data/master_table.rds")
-#rownames(master) <- master$gene_symbol
-
-# input <- list()
-# input$gene_symbol <- c("A2ML1", "A2M") #c("ASTN1", "A1CF")
+gene_symbol <- read.fst("data/gene_symbol.fst")
+gnomad_exome <- read.fst("data/gnomad_exome.fst")
+summary <- read.fst("data/summary.fst")
+gtrM <- read.fst("data/gtrM.fst")
+gtrS <- read.fst("data/gtrS.fst")
+gpt <- read.fst("data/gpt.fst")
+#genes_by_ccds_id <- read.fst("data/genes_by_ccds_id.fst")
 
 # Define UI ----
 ui <- fluidPage(
+  useShinyjs(),
   #theme = shinytheme("cerulean"),
-  # useShinyjs(),
-  titlePanel("WEScover"),
-  fluidRow(
-    column(2,
-        wellPanel(
-           h2("User input"),
-           # radioButtons("type_input", 
-           #              label = "Type of input", 
-           #              choices = c("Gene symbol", "Phenotype"),
-           #              selected = "Gene symbol",
-           #              inline = TRUE),
-           selectizeInput("gene_symbol", 
-                          label = "Gene symbol",
-                          choices = gene_symbol,
-                          multiple = TRUE ),
-           selectizeInput("phenotype",
-                          label="Phenotype",
-                          choices = sort(unique(pheno$phenotype_name)),
-                          multiple = TRUE),
-           selectInput("depth_of_coverage", 
-                       label = "Depth of coverage",
-                       choices = c("10x", "20x", "30x"),
-                       selected = "20x"),
-           #submitButton("Submit query")
-           actionButton("update", "Submit query")
-    )),
-    column(10,
-           dataTableOutput('tableMain')  
-    )#,
-    # column(5, 
-    #        plotlyOutput("plot")#, height = 650)
-    #        #plotOutput("plot")
-    # )
-  ),
-  hr(),
-  fluidRow(
-    column(3),
-    column(6),
-    column(3)
-  )
+  #titlePanel("WEScover"),
+  navbarPage("WEScover",
+    tabPanel("Report",
+      sidebarLayout(position = "left",
+      sidebarPanel(
+              tags$h2("User input"),
+              selectizeInput("gene_symbol",
+                              label = "Gene symbol",
+                              choices = NULL,
+                              multiple = TRUE),
+              selectizeInput("gpt",
+                              label="Test name",
+                              choices = NULL,
+                              multiple = TRUE),
+               selectInput("depth_of_coverage", 
+                           label = "Depth of coverage",
+                           choices = c("10x", "20x", "30x"),
+                           selected = "20x")
+               ,
+               actionButton("update", "Submit query"),
+               actionButton("resetInputs", "Clear inputs")
+        ),
+        mainPanel(
+               dataTableOutput('tableMain')  
+        )
+      )
+    ),
+    tabPanel("Help"
+    )
+  )#,
+  # tags$footer(title="© Boston Children's Hospital. All Rights Reserved.", 
+  #   align = "right", 
+  #   style = "
+  #     position:absolute;
+  #     bottom:0;
+  #     width:100%;
+  #     height:50px; /* Height of the footer */
+  #     color: white;
+  #     padding: 10px;
+  #     background-color: #DCDCDC;
+  #     z-index: 1000;"
+  # )
 )
 
 
 # Define server logic ----
-server <- function(input, output) {
+server <- function(input, output, session) {
+  
+  updateSelectizeInput(session, 'gene_symbol', choices = gene_symbol$gene_symbol, server = TRUE)
+  updateSelectizeInput(session, 'gpt', choices =sort(unique(gpt$test_name)), server = TRUE)
   
   # generator of buttons for the main table
   shinyInput <- function(FUN, len, id, ...) {
@@ -87,7 +90,8 @@ server <- function(input, output) {
   
   
   # list of global values
-  myValue <- reactiveValues(summary_table = NA, GPT_table = NA, violon_population = NA, gene = "", ccds="")
+  myValue <- reactiveValues(summary_table = NA, GPT_table = NA, 
+    violon_population = NA, gene = "", ccds="", gnomAD_plot="")
   
   
   # observer to capture detail buttons in the main table
@@ -98,14 +102,28 @@ server <- function(input, output) {
     
     myValue$summary_table <<- 
       createMainTable2(geneS, input$depth_of_coverage, summary, gtrM, gtrS)[,c(1,2,6:10)]
-    message("MODAL:",colnames(myValue$summary_table))
     myValue$GPT_table <<- 
       createGPT(selectedRow, main_table(), gtrM)
-    message("MODAL:",colnames(myValue$GPT_table))
     myValue$violon_population <<- createPlot(geneS, main_table()[selectedRow, 2])
     myValue$gene <<- geneS
     myValue$ccds <<- main_table()[selectedRow, 2]
-    message("FN")
+    fileAD <- 
+      paste0("/opt/shiny-server/samples/sample-apps/WEScover/coverage_plots/", myValue$ccds, ".png")
+    if(file.exists(fileAD)) {
+      myValue$gnomAD_plot <<- list(
+        src = fileAD,
+        contentType = 'image/png',
+        width = "100%",
+        #height = 300,
+        alt = paste0("gnomAD coverage for ", myValue$ccds))
+    } else {
+      myValue$gnomAD_plot <<- list(
+        src = "/opt/shiny-server/samples/sample-apps/WEScover/coverage_plots/NO_CCDS_GAD.png",
+        contentType = 'image/png',
+        width = "100%",
+        #height = 300,
+        alt = paste0("No coverage from gnomAD for ", myValue$ccds))
+    }
     showModal(modal_main())
     
   })
@@ -117,8 +135,8 @@ server <- function(input, output) {
       geneS <- c(geneS, input$gene_symbol)
     }
     
-    if (length(input$phenotype) != 0) {
-      geneP <- as.character(unique(pheno[pheno$phenotype_name == input$phenotype, "gene_symbol"]))
+    if (length(input$gpt) != 0) {
+      geneP <- as.character(unique(gpt[gpt$test_name == input$gpt, "gene_symbol"]))
       geneS <- c(geneS, geneP)
     }
     
@@ -126,7 +144,9 @@ server <- function(input, output) {
     tbl <- createMainTable2(geneS, input$depth_of_coverage, summary, gtrM, gtrS)
     if(ncol(tbl) > 0) {
       tbl <- tbl[ , seq(5)]
-      tbl$Action <- shinyInput(actionButton, nrow(tbl), 'button_', label = "Detail", onclick = 'Shiny.onInputChange(\"detail_button\",  this.id)' )
+      tbl$Action <- shinyInput(actionButton, nrow(tbl), 'button_', 
+                               label = "Detail", 
+                               onclick = 'Shiny.onInputChange(\"detail_button\",  this.id)' )
     }
     tbl
   })
@@ -137,7 +157,7 @@ server <- function(input, output) {
     isolate({
       if(ncol(main_table()) > 0) {
         formatStyle(datatable(main_table(), escape = FALSE, selection = 'none'), 
-                  columns = "Global mean", target = 'row', backgroundColor = 
+                  columns = "Global coverage (mean, %)", target = 'row', backgroundColor = 
                     styleInterval(cuts = c(95, 98), values=c("#FFE4E1", "#FFFFE0", "#F0FFF0")))
       }
     })
@@ -145,14 +165,16 @@ server <- function(input, output) {
   
   # modal window to show tables and plots
   modal_main <- function(failed = FALSE){
-    modalDialog(
+    modalDialog(size="l",
       tabsetPanel(type="tabs",
         tabPanel("Population summary",
           dataTableOutput('summary_table')),
-        tabPanel("Gene panels",
-                 dataTableOutput('GPT_table')),
         tabPanel("Violin per population",
-                 plotOutput("violin_population"))
+                 plotOutput("violin_population")),
+        tabPanel("gnomAD coverage",
+                 imageOutput("gnomAD_plot")),
+        tabPanel("Gene panels",
+                 dataTableOutput('GPT_table'))
       ),
       easyClose = TRUE
     )
@@ -164,15 +186,16 @@ server <- function(input, output) {
   }, escape = FALSE)
   
   output$violin_population <- renderPlot({
-    message("RENDER")
-    #myValue$violin_population
     createPlot(myValue$gene, myValue$ccds)
   })
   
   output$GPT_table <- renderDataTable({
-    message("TBL")
     myValue$GPT_table
   }, escape = FALSE)
+  
+  output$gnomAD_plot <- renderImage({
+    myValue$gnomAD_plot
+  }, deleteFile = FALSE)
   
   
   
@@ -219,15 +242,13 @@ server <- function(input, output) {
     dta$value <- as.numeric(dta$value)
     
     gAD <- gnomad_exome[selCCDS, idx]
-    message("AB")
-    p1 <<- ggplot(dta, aes(x=Population, y=value, color=Population)) + 
+    p1 <- ggplot(dta, aes(x=Population, y=value, color=Population)) + 
       theme_bw() + geom_violin(outlier.shape = NA)  +
       facet_wrap(GeneSymbol~CCDS) +
       geom_hline(yintercept=gAD, colour="black", show.legend = T) +
       scale_y_continuous(labels = function(x) paste0(x*100, "%")) +
       ylab("Breadth of coverage")
     
-    message("CD")
     #ggplotly(p1)
     p1
   }
--- function.R
+++ function.R
@@ -1,4 +1,4 @@
-genes_by_ccds_id <- readRDS('../data/genes_by_ccds_id.rds')
+genes_by_ccds_id <- readRDS('data/genes_by_ccds_id.rds')
 
 getCCDS <- function(geneN, sm) {
   #message("getCCDS - ", paste(geneN, collapse = ", "))
@@ -9,23 +9,23 @@ getCCDS <- function(geneN, sm) {
 
 load10x <- function(summary) {
   if(!"AFR_10x" %in% ls(envir = .GlobalEnv)) {
-    AFR_10x <- readRDS("../data/AFR_10x.rds")
+    AFR_10x <- readRDS("data/AFR_10x.rds")
     AFR_10x$CCDS <- rownames(AFR_10x)
     AFR_10x$Population <- "AFR"
     AFR_10x$GeneSymbol <- summary[AFR_10x$CCDS, "gene"]
-    AMR_10x <- readRDS("../data/AMR_10x.rds")
+    AMR_10x <- readRDS("data/AMR_10x.rds")
     AMR_10x$CCDS <- rownames(AMR_10x)
     AMR_10x$Population <- "AMR"
     AMR_10x$GeneSymbol <- summary[AMR_10x$CCDS, "gene"]
-    EAS_10x <- readRDS("../data/EAS_10x.rds")
+    EAS_10x <- readRDS("data/EAS_10x.rds")
     EAS_10x$CCDS <- rownames(EAS_10x)
     EAS_10x$Population <- "EAS"
     EAS_10x$GeneSymbol <- summary[EAS_10x$CCDS, "gene"]
-    EUR_10x <- readRDS("../data/EUR_10x.rds")
+    EUR_10x <- readRDS("data/EUR_10x.rds")
     EUR_10x$CCDS <- rownames(EUR_10x)
     EUR_10x$Population <- "EUR"
     EUR_10x$GeneSymbol <- summary[EUR_10x$CCDS, "gene"]
-    SAS_10x <- readRDS("../data/SAS_10x.rds")
+    SAS_10x <- readRDS("data/SAS_10x.rds")
     SAS_10x$CCDS <- rownames(SAS_10x)
     SAS_10x$Population <- "SAS"
     SAS_10x$GeneSymbol <- summary[SAS_10x$CCDS, "gene"]
@@ -40,23 +40,23 @@ load10x <- function(summary) {
 
 load20x <- function(summary) {
   if(!"AFR_20x" %in% ls(envir = .GlobalEnv)) {
-    AFR_20x <- readRDS("../data/AFR_20x.rds")
+    AFR_20x <- readRDS("data/AFR_20x.rds")
     AFR_20x$CCDS <- rownames(AFR_20x)
     AFR_20x$Population <- "AFR"
     AFR_20x$GeneSymbol <- summary[AFR_20x$CCDS, "gene"]
-    AMR_20x <- readRDS("../data/AMR_20x.rds")
+    AMR_20x <- readRDS("data/AMR_20x.rds")
     AMR_20x$CCDS <- rownames(AMR_20x)
     AMR_20x$Population <- "AMR"
     AMR_20x$GeneSymbol <- summary[AMR_20x$CCDS, "gene"]
-    EAS_20x <- readRDS("../data/EAS_20x.rds")
+    EAS_20x <- readRDS("data/EAS_20x.rds")
     EAS_20x$CCDS <- rownames(EAS_20x)
     EAS_20x$Population <- "EAS"
     EAS_20x$GeneSymbol <- summary[EAS_20x$CCDS, "gene"]
-    EUR_20x <- readRDS("../data/EUR_20x.rds")
+    EUR_20x <- readRDS("data/EUR_20x.rds")
     EUR_20x$CCDS <- rownames(EUR_20x)
     EUR_20x$Population <- "EUR"
     EUR_20x$GeneSymbol <- summary[EUR_20x$CCDS, "gene"]
-    SAS_20x <- readRDS("../data/SAS_20x.rds")
+    SAS_20x <- readRDS("data/SAS_20x.rds")
     SAS_20x$CCDS <- rownames(SAS_20x)
     SAS_20x$Population <- "SAS"
     SAS_20x$GeneSymbol <- summary[SAS_20x$CCDS, "gene"]
@@ -71,23 +71,23 @@ load20x <- function(summary) {
 
 load30x <- function(summary) {
   if(!"AFR_30x" %in% ls(envir = .GlobalEnv)) {
-    AFR_30x <- readRDS("../data/AFR_30x.rds")
+    AFR_30x <- readRDS("data/AFR_30x.rds")
     AFR_30x$CCDS <- rownames(AFR_30x)
     AFR_30x$Population <- "AFR"
     AFR_30x$GeneSymbol <- summary[AFR_30x$CCDS, "gene"]
-    AMR_30x <- readRDS("../data/AMR_30x.rds")
+    AMR_30x <- readRDS("data/AMR_30x.rds")
     AMR_30x$CCDS <- rownames(AMR_30x)
     AMR_30x$Population <- "AMR"
     AMR_30x$GeneSymbol <- summary[AMR_30x$CCDS, "gene"]
-    EAS_30x <- readRDS("../data/EAS_30x.rds")
+    EAS_30x <- readRDS("data/EAS_30x.rds")
     EAS_30x$CCDS <- rownames(EAS_30x)
     EAS_30x$Population <- "EAS"
     EAS_30x$GeneSymbol <- summary[EAS_30x$CCDS, "gene"]
-    EUR_30x <- readRDS("../data/EUR_30x.rds")
+    EUR_30x <- readRDS("data/EUR_30x.rds")
     EUR_30x$CCDS <- rownames(EUR_30x)
     EUR_30x$Population <- "EUR"
     EUR_30x$GeneSymbol <- summary[EUR_30x$CCDS, "gene"]
-    SAS_30x <- readRDS("../data/SAS_30x.rds")
+    SAS_30x <- readRDS("data/SAS_30x.rds")
     SAS_30x$CCDS <- rownames(SAS_30x)
     SAS_30x$Population <- "SAS"
     SAS_30x$GeneSymbol <- summary[SAS_30x$CCDS, "gene"]
@@ -225,45 +225,45 @@ createGPT <- function(row, main_table, gtrM) {
   tbl
 }
 
-createMainTable <- function(geneS, depth, summary, gtrM, gtrS) {
-  selCCDS <- getCCDS(geneS, summary)
-
-  message("[TABLE] Number of CCDS: ", length(selCCDS), "; Number of genes:", length(geneS) )
-  idx <- ifelse(depth == "10x", 3, ifelse(depth == "20x", 4, 5))
-  xx <- do.call(rbind, lapply(geneS, function(gene) {
-    message(gene)
-    selCCDS <- getCCDS(gene, summary)
-    if(length(selCCDS) > 0) {
-      GS <- gtrS[selCCDS, "gene"]
-      GPT <- length(unique(gtrM[gtrM$GeneSymbol == gene, "AccessionVersion"]))
-      MM <- globalMean(idx, selCCDS, summary)
-      data.frame(
-        A = GS,
-        B = selCCDS,
-        C = GPT,
-        #D = paste(round(MM$GLOBAL * 100, 0), " (", round(MM$GLOBAL.MI * 100, 0), "-", round(MM$GLOBAL.MA * 100, 0), ")", sep=""),
-        D = round(MM$GLOBAL * 100, 0),
-        E = paste(round(MM$AFR * 100, 0), " (", round(MM$AFR.MI * 100, 0), "-", round(MM$AFR.MA * 100, 0), ")", sep=""),
-        F = paste(round(MM$AMR * 100, 0), " (", round(MM$AMR.MI * 100, 0), "-", round(MM$AMR.MA * 100, 0), ")", sep=""),
-        G = paste(round(MM$EAS * 100, 0), " (", round(MM$EAS.MI * 100, 0), "-", round(MM$EAS.MA * 100, 0), ")", sep=""),
-        H = paste(round(MM$EUR * 100, 0), " (", round(MM$EUR.MI * 100, 0), "-", round(MM$EUR.MA * 100, 0), ")", sep=""),
-        I = paste(round(MM$SAS * 100, 0), " (", round(MM$SAS.MI * 100, 0), "-", round(MM$SAS.MA * 100, 0), ")", sep=""),
-        stringsAsFactors = FALSE
-      )
-    } else {
-      data.frame(
-        A = gene, B = NA, C = NA, D = NA,
-        E = NA, F = NA, G = NA, H = NA,
-        I = NA, stringsAsFactors = FALSE
-      )
-    }
-  }))
-  colnames(xx) <- c("Gene Symbol", "CCDS", "Gene Panel Testing", "GM", "AFR (%)", 
-                    "AMR (%)", "EAS (%)", "EUR (%)", "SAS (%)")
-  rownames(xx) <- seq(nrow(xx))
-  xx
-
-}
+# createMainTable <- function(geneS, depth, summary, gtrM, gtrS) {
+#   selCCDS <- getCCDS(geneS, summary)
+# 
+#   message("[TABLE] Number of CCDS: ", length(selCCDS), "; Number of genes:", length(geneS) )
+#   idx <- ifelse(depth == "10x", 3, ifelse(depth == "20x", 4, 5))
+#   xx <- do.call(rbind, lapply(geneS, function(gene) {
+#     message(gene)
+#     selCCDS <- getCCDS(gene, summary)
+#     if(length(selCCDS) > 0) {
+#       GS <- gtrS[selCCDS, "gene"]
+#       GPT <- length(unique(gtrM[gtrM$GeneSymbol == gene, "AccessionVersion"]))
+#       MM <- globalMean(idx, selCCDS, summary)
+#       data.frame(
+#         A = GS,
+#         B = selCCDS,
+#         C = GPT,
+#         #D = paste(round(MM$GLOBAL * 100, 0), " (", round(MM$GLOBAL.MI * 100, 0), "-", round(MM$GLOBAL.MA * 100, 0), ")", sep=""),
+#         D = round(MM$GLOBAL * 100, 0),
+#         E = paste(round(MM$AFR * 100, 0), " (", round(MM$AFR.MI * 100, 0), "-", round(MM$AFR.MA * 100, 0), ")", sep=""),
+#         F = paste(round(MM$AMR * 100, 0), " (", round(MM$AMR.MI * 100, 0), "-", round(MM$AMR.MA * 100, 0), ")", sep=""),
+#         G = paste(round(MM$EAS * 100, 0), " (", round(MM$EAS.MI * 100, 0), "-", round(MM$EAS.MA * 100, 0), ")", sep=""),
+#         H = paste(round(MM$EUR * 100, 0), " (", round(MM$EUR.MI * 100, 0), "-", round(MM$EUR.MA * 100, 0), ")", sep=""),
+#         I = paste(round(MM$SAS * 100, 0), " (", round(MM$SAS.MI * 100, 0), "-", round(MM$SAS.MA * 100, 0), ")", sep=""),
+#         stringsAsFactors = FALSE
+#       )
+#     } else {
+#       data.frame(
+#         A = gene, B = NA, C = NA, D = NA,
+#         E = NA, F = NA, G = NA, H = NA,
+#         I = NA, stringsAsFactors = FALSE
+#       )
+#     }
+#   }))
+#   colnames(xx) <- c("Gene Symbol", "CCDS", "Gene Panel Testing", "GM", "AFR (%)", 
+#                     "AMR (%)", "EAS (%)", "EUR (%)", "SAS (%)")
+#   rownames(xx) <- seq(nrow(xx))
+#   xx
+# 
+# }
 
 # Jeff's versions
 
@@ -313,7 +313,7 @@ createMainTable2 <- function(geneS, depth, summary, gtrM, gtrS) {
     }
     xx <- genes_by_ccds_id[genes_by_ccds_id$gene_symbol %in% geneS, colS]
     xx <- xx[order(xx[, 3]), ]
-    colnames(xx) <- c("Gene Symbol", "CCDS", "Global mean", "Global min", "Global max", "AFR (%)", 
+    colnames(xx) <- c("Gene Symbol", "CCDS", "Global coverage (mean, %)", "Global coverage (min, %)", "Global coverage (max, %)", "AFR (%)", 
                       "AMR (%)", "EAS (%)", "EUR (%)", "SAS (%)")
     rownames(xx) <- seq(nrow(xx))
     xx
